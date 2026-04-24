9da967e29829639c96adf3bbf9122cd16cbbfdd5
azhnhakimi <azhanhakimii@gmail.com>
2026-04-24 11:27:30 +0800

    removed duplicate auth checks

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,9 +1,5 @@
 "use client";
-
-import { useEffect, useState, ReactNode } from "react";
-import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
-
+import { ReactNode } from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/navigation/app-sidebar";
 
@@ -12,36 +8,6 @@ interface ProtectedLayoutProps {
 }
 
 export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
-  const supabase = createClient();
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const checkSession = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (!session?.user) {
-        router.push("/login");
-      } else {
-        setLoading(false);
-      }
-    };
-
-    checkSession();
-  }, [router, supabase]);
-
-  if (loading) {
-    return (
-      <SidebarProvider>
-        <AppSidebar />
-        <div className="flex items-start justify-start min-h-screen h-full w-full p-6">
-          <p>Loading...</p>
-        </div>
-      </SidebarProvider>
-    );
-  }
-
   return (
     <SidebarProvider>
       <AppSidebar />
